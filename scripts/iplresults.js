f9ea4f001151fe70313c686f2877a666b1563786
Ravikirar712 <ravikirar712@gmail.com>
2021-11-12 16:20:36 +0530

    msdhoni

---
+++ scripts/iplresults.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAWh_epOHcjM-H4mAnlmYXn_QBzr7dbm1U",
+  authDomain: "ms-dhoni-39c76.firebaseapp.com",
+  databaseURL: "https://ms-dhoni-39c76-default-rtdb.firebaseio.com",
+  projectId: "ms-dhoni-39c76",
+  storageBucket: "ms-dhoni-39c76.appspot.com",
+  messagingSenderId: "556242799671",
+  appId: "1:556242799671:web:1ebfeeb33857b9270b03b2"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
